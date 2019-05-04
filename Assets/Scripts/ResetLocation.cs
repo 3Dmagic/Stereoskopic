a107f921fe7a3a9228d648d89b8541b276bcfbd1
InvisibleRoom <sascha@invisibleroom.com>
2019-05-04 11:35:39 +0200

    Reset Location based on leftController

---
+++ Assets/Scripts/ResetLocation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLocation : MonoBehaviour
+{
+    public GameObject Controller;
+    public GameObject SceneObject;
+
+    public void ResetObjectLocation()
+    {
+        Vector3 referenceLocation = Controller.transform.position;
+        SceneObject.transform.position = new Vector3(referenceLocation.x, 0, referenceLocation.z);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("ResetLocation"))
+        {
+            if(Controller == null || !Controller.activeSelf) { Debug.LogWarning("No Controller in scene"); return; }
+            ResetObjectLocation();
+        }
+    }
+
+
+}
